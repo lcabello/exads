baa9b0afadc3b0c1c0c20687eebab37af4effb54
Luis Cabello <lcabello@Luiss-MacBook-Pro.local>
2023-07-25 16:30:25 +0200

    feat: Update typedf and resolvers with correct info

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,7 +1,98 @@
+import dynamodb from '../db/dynamoDB.js';
+
+const DB_NAME = 'ExadsMetaData';
 const resolvers = {
-	Query: {
-		helloWorld: () => `Hello world`,
-	},
+  Query: {
+    async getWebPage(_parent, { url }) {
+      try {
+        const params = {
+          ExpressionAttributeValues: {
+            ':url': url
+          },
+          KeyConditionExpression: 'URL = :url',
+          TableName: DB_NAME
+        };
+        const result = await dynamodb.query(params).promise();
+
+        return result;
+      } catch (e) {
+        console.log("Error", e);
+        return e;
+      }
+    },
+    async getWebPageByField(_parent, { field, value }) {
+      try {
+        const params = {
+          ExpressionAttributeValues: {
+            ':field': value
+          },
+          ExpressionAttributeNames: {
+            '#field': field,
+          },
+          KeyConditionExpression: 'URL = :url',
+          TableName: DB_NAME
+        };
+        const result = await dynamodb.query(params).promise();
+
+        return result;
+      } catch (e) {
+        console.log("Error", e);
+        return e;
+      }
+    }
+  },
+  Mutation: {
+    createItem: async (_parent, params) => {
+      try {
+        const paramsDB = {
+          TableName: DB_NAME,
+          Item: params.Item
+        };
+        await dynamodb.putItem(paramsDB).promise();
+
+        return params.Item;
+      } catch (e) {
+        console.log("Error", e);
+        return e;
+      }
+
+    },
+    updateItem: async (_parent, { url, field, value }) => {
+      try {
+        const params = {
+          TableName: DB_NAME,
+          Key: { url },
+          UpdateExpression: 'SET #name = :name',
+          ExpressionAttributeNames: {
+            '#name': field
+          },
+          ExpressionAttributeValues: {
+            ':name': value,
+          },
+          ReturnValues: 'ALL_NEW',
+        };
+        const { Attributes } = await dynamodb.update(params).promise();
+        return Attributes;
+      } catch(e) {
+        console.log("Error", e);
+        return e;
+      }
+
+    },
+    deleteItem: async (_parent, { url }) => {
+      try {
+        const params = {
+          TableName: DB_NAME,
+          Key: { url }
+        };
+        await dynamodb.delete(params).promise();
+        return true;
+      } catch(e) {
+        console.log("Error", e);
+        return e;
+      }
+    },
+  }
 };
 
 export default resolvers;
--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,6 +1,28 @@
 const typeDefs = `
+  type WebPage {
+    id: ID!
+    url: String!
+    title: String!
+    date: String!
+    countWords: Number!
+  }
+
   type Query {
-    helloWorld: String!
+    getWebPage(id: ID!): WebPage
+    getWebPageByField(field: String!, value: String!): WebPage
+  }
+
+  input WebPageInput {
+    url: String!
+    title: String!
+    date: String!
+    countWords: Number!
+  }
+
+  type Mutation {
+    createWebPage(input: WebPageInput!): WebPage
+    updateWebPage(id: ID!, input: WebPageInput!): WebPage
+    deleteWebPage(id: ID!): ID
   }
 `;
 
